7b35c384d22c60b12ca5a0caa3cb397f80749983
snehagowda17 <aradhanagowda17@gmail.com>
2026-04-01 11:27:56 +0530

    Refactor Flask routes for posts, comments, and albums

--- app.py
+++ app.py
@@ -1,49 +1,45 @@
-from flask import Flask, jsonify
 import requests
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
-BASE_URL = "https://jsonplaceholder.typicode.com"
+@app.route('/posts')
+def get_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    payload = {}
+    headers = {}
 
-
-@app.route("/")
-def home():
+    response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
-        "message": "Flask JSONPlaceholder API is running",
-        "routes": ["/posts", "/comment", "/albums"]
-        "data":["successfully committed"]
+        "data": response.json(),
+        "status": "successfully triggered auto deployment",
+        "status_code": 200
     })
 
-
-@app.route("/posts")
-def get_posts():
-    try:
-        response = requests.get(f"{BASE_URL}/posts")
-        response.raise_for_status()
-        return jsonify(response.json())
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
-
-
-@app.route("/comment")
+@app.route('/comments')
 def get_comments():
-    try:
-        response = requests.get(f"{BASE_URL}/comments")
-        response.raise_for_status()
-        return jsonify(response.json())
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
+    url = "https://jsonplaceholder.typicode.com/comments"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
 
-
-@app.route("/albums")
+@app.route('/albums')
 def get_albums():
-    try:
-        response = requests.get(f"{BASE_URL}/albums")
-        response.raise_for_status()
-        return jsonify(response.json())
-    except requests.exceptions.RequestException as e:
-        return jsonify({"error": str(e)}), 500
+    url = "https://jsonplaceholder.typicode.com/albums"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return jsonify({
+        "data": response.json(),
+        "status": "success",
+        "status_code": 200
+    })
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
